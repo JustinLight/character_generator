a0aa07c31f076fff3771bd3e6cf86c302cac92d2
Justin Light <justinlightsaber@gmail.com>
2018-03-05 14:19:41 -0800

    added .json files for races, classes, feats, and spellcasting.

--- randomDie.py
+++ randomDie.py
@@ -5,7 +5,7 @@ d={}
 randomfile=glob2.glob('class_'+random.choice(["fighter", "wizard"])+'.txt').pop()
 with open(randomfile) as f:
     for line in f:
-        (key, val) = line.split()
+        (key, val) = line.split('/')
         d[key] = val
 
 statArray = random.choices([3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18], [0.1, 0.3, 0.8, 1.6, 2.9, 4.8, 7, 9.4, 11.4, 12.9, 13.3, 12.3, 10.1, 7.3, 4.2, 1.6], k=6)
@@ -13,6 +13,8 @@ statName = ["Strength ","Dexterity ","Constitution ","Intelligence ","Wisdom ","
 statBonus =[]
 for i in statArray:
     statBonus.append(math.floor((i-10)/2))
+
+print(d)
 
 file=open("RandomCharacter.txt",'w')
 file.write('Class '+d['class']+"\n")
